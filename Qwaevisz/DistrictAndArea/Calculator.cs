cad72dec84d2d78e047e6cc36b644c076d0a0afe
qwaevisz <qwaevisz@gmail.com>
2021-10-07 07:55:55 +0200

    Add DistrictAndArea project

---
+++ Qwaevisz/DistrictAndArea/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DistrictAndArea
+{
+    public class Calculator
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Téglalap A oldala (cm): ");
+            string sideAText = Console.ReadLine();
+            Console.Write("Téglalap B oldala (cm): ");
+            string sideBTest = Console.ReadLine();
+
+            int sideA = Convert.ToInt32(sideAText);
+            int sideB = Convert.ToInt32(sideBTest);
+
+            int district = 2 * sideA + 2 * sideB;
+            int area = sideA * sideB;
+
+            Console.WriteLine("A megadott téglalap kerülete " + district + " cm, területe pedig " + area + " cm2.");
+        }
+    }
+}
