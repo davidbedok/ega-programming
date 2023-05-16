c5f2e4856ae6e7d0a1fa15e01454b16bb3685112
qwaevisz <qwaevisz@gmail.com>
2023-05-16 14:17:30 +0200

    Add initial version of Execptions project

---
+++ QwaeviszOOP/Exceptions/CustomNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+    public class CustomNotFoundException : Exception
+    {
+
+        public CustomNotFoundException()
+        {
+        }
+
+        public CustomNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public CustomNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+    }
+}
---
+++ QwaeviszOOP/Exceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exceptions
+{
+    public class Program
+    {
+
+        private void Hello()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program p = null;
+                p.Hello();
+                throw new CustomNotFoundException("Hello", new NullReferenceException());
+            } catch ( CustomNotFoundException e )
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.StackTrace);
+            } catch (Exception e )
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.StackTrace);
+            }
+
+}
+    }
+}
